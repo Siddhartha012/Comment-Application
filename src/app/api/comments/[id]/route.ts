59cd10f2a5bde99905bd7955dfe7c179b6367c3a
Siddhartha012 <siddharthamaity01@gmail.com>
2025-07-07 09:22:20 +0530

    import NextRequest in refresh route

--- src/app/api/comments/[id]/route.ts
+++ src/app/api/comments/[id]/route.ts
@@ -1,5 +1,5 @@
 
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/db';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/lib/auth';
