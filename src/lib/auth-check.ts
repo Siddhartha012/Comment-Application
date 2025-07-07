ec5621a581bcf2981a8e45ec68c4096219494fc1
Siddhartha012 <siddharthamaity01@gmail.com>
2025-07-07 17:31:53 +0530

    updating session usage 2

--- src/lib/auth-check.ts
+++ src/lib/auth-check.ts
@@ -2,10 +2,10 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "./auth";
 
-//import { Session } from "next-auth";
+import { Session } from "next-auth";
 
 export async function requireSession() {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions) as Session | null;
   if (!session || !session.user) {
     throw new Error("Unauthorized");
   }
